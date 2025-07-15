44f428098e813bc68fc4ce64196e6990d49e2ceb
Helmi BA <helmiabdelghani00@gmail.com>
2025-07-15 23:27:21 +0100

    modified the test.js

--- test.js
+++ test.js
@@ -1,6 +1,16 @@
 const test = require('node:test');
 const assert = require('node:assert');
 const http = require('http');
+const express=require('express')
+
+
+const app=express()
+const users = ["helmi", "hamma", "salma"];
+app.get("/", (req, res) => res.status(200).json(users));
+
+const server = app.listen(3000, () => {
+  console.log("Test server running... on port 3000");
+});
 
 test('GET / should return JSON array of users', async (t) => {
   const res = await new Promise((resolve) => {
@@ -16,4 +26,6 @@ test('GET / should return JSON array of users', async (t) => {
   const expected = ["helmi", "hamma", "salma"];
 
   assert.deepStrictEqual(data, expected);
+
+  server.close()
 });
